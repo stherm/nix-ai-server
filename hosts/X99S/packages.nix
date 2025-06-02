8ec24ea121f46784504b034eb21f9526cf89aa8f
stherm <steffen_hermann97@yahoo.de>
2025-06-02 11:05:13 +0200

    removed marker for faster building

--- hosts/X99S/packages.nix
+++ hosts/X99S/packages.nix
@@ -2,9 +2,10 @@
 
 {
   environment.systemPackages = with pkgs; [
-    core.marker-pdf
+    #core.marker-pdf
     dotnetCorePackages.dotnet_8.runtime
     dotnetCorePackages.sdk_8_0_3xx
     gitingest
   ];
 }
+
