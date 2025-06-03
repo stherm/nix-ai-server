76159b6b6cacd5b3a10afd7803a0dd85d3804880
sid <sid@portuus.de>
2025-06-03 07:43:53 +0200

    using portuus as binary cache server

--- hosts/X99S/packages.nix
+++ hosts/X99S/packages.nix
@@ -2,10 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    #core.marker-pdf
+    # core.marker-pdf
     dotnetCorePackages.dotnet_8.runtime
     dotnetCorePackages.sdk_8_0_3xx
     gitingest
   ];
 }
-
