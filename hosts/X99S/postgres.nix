160bae850192e8c51756b81898182b4a85596cc3
stherm <steffen_hermann97@yahoo.de>
2025-06-06 12:19:40 +0200

    added pg admin

--- hosts/X99S/postgres.nix
+++ hosts/X99S/postgres.nix
@@ -1,18 +1,26 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   pgDataDir = "/var/lib/postgresql/${config.services.postgresql.package.psqlSchema}";
-  domain   = config.networking.domain;
+  domain = config.networking.domain;
   hostname = config.networking.hostName;
-  fqdn     = "${hostname}.${domain}";
+  fqdn = "${hostname}.${domain}";
 in
 {
   systemd.services.postgresql-cert-generator = {
     description = "PostgreSQL Certificate Generator";
-    wantedBy    = [ "postgresql.service" ];
-    after       = [ "postgresql.service" ];
-    path        = with pkgs; [ openssl coreutils ];
-    script      = ''
+    wantedBy = [ "postgresql.service" ];
+    after = [ "postgresql.service" ];
+    path = with pkgs; [
+      openssl
+      coreutils
+    ];
+    script = ''
       CERT_FILE="${pgDataDir}/server.crt"
       KEY_FILE="${pgDataDir}/server.key"
       NEEDS_NEW=0
@@ -46,60 +54,85 @@ in
   };
 
   systemd.timers.postgresql-cert-generator = {
-    wantedBy    = [ "timers.target" ];
+    wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar         = "monthly";
-      Persistent         = true;
+      OnCalendar = "monthly";
+      Persistent = true;
       RandomizedDelaySec = "12h";
     };
   };
 
-services.postgresql = {
-    enable           = true;
-    package          = pkgs.postgresql_16;
-    dataDir          = "/var/lib/postgresql/16";
-    enableTCPIP      = true;
-    settings.port    = 5432;
-    ensureDatabases  = [ "fw_grafschaft" "pnp" ];
-    settings         = { ssl = true; };
-    ensureUsers	     = [
-	{name = "tobi";
-	ensureClauses = {
-	login = true;
-	superuser = true;
-	};
-	}
-	{name = "steffen";
-	ensureClauses = {
-	login = true;
-	superuser = true;
-	};
-	}
-	{name = "peter";
-	ensureClauses = {
-	login = true;
-	superuser = true;
-	};
-	}
-	{name = "pnp";
-	ensureClauses = {
-	login = true;
-	};
-	}
-	{name = "fw_grafschaft";
-	ensureClauses = {
-	login = true;
-	};
-	}
+  ervices.pgadmin = {
+    enable = true;
+    initialEmail = "steffen@portuus.de";
+    initialPasswordFile = "/home/steffen/pgpass";
+  };
+
+  services.nginx.virtualHosts."sql.steffen.fail" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://localhost:5050";
+      proxyWebsockets = true;
+    };
+  };
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    dataDir = "/var/lib/postgresql/16";
+    enableTCPIP = true;
+    settings.port = 5432;
+    ensureDatabases = [
+      "fw_grafschaft"
+      "pnp"
     ];
-    authentication   = lib.mkOverride 10 ''
+    settings = {
+      ssl = true;
+    };
+    ensureUsers = [
+      {
+        name = "tobi";
+        ensureClauses = {
+          login = true;
+          superuser = true;
+        };
+      }
+      {
+        name = "steffen";
+        ensureClauses = {
+          login = true;
+          superuser = true;
+        };
+      }
+      {
+        name = "peter";
+        ensureClauses = {
+          login = true;
+          superuser = true;
+        };
+      }
+      {
+        name = "pnp";
+        ensureClauses = {
+          login = true;
+        };
+      }
+      {
+        name = "fw_grafschaft";
+        ensureClauses = {
+          login = true;
+        };
+      }
+    ];
+    authentication = lib.mkOverride 10 ''
       local all       all                trust
       # Zugriffe für fw_grafschaft
       hostssl fw_grafschaft prosinsky     0.0.0.0/0   scram-sha-256
       hostssl fw_grafschaft prosinsky     ::/0        scram-sha-256
       hostssl fw_grafschaft postgres      0.0.0.0/0   scram-sha-256
       hostssl fw_grafschaft postgres      ::/0        scram-sha-256
-      
+
       # Zugriffe für pnp
       hostssl pnp     tobi          0.0.0.0/0   scram-sha-256
       hostssl pnp     tobi          ::/0        scram-sha-256
@@ -110,8 +143,7 @@ services.postgresql = {
       hostssl pnp     pnp      0.0.0.0/0        scram-sha-256
       hostssl pnp     pnp      ::/0        scram-sha-256
     '';
-};
+  };
   networking.firewall.allowedTCPPorts = [ 5432 ];
-  users.users.postgres.extraGroups      = [ "nginx" ];
+  users.users.postgres.extraGroups = [ "nginx" ];
 }
-
