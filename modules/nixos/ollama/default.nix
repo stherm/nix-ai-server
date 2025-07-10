8d264048d5c2984639706aa52be0711e74e5ca20
sid <sid@portuus.de>
2025-07-10 11:43:47 +0200

    added nginx reverse proxy to ollama module

--- modules/nixos/ollama/default.nix
+++ modules/nixos/ollama/default.nix
@@ -2,16 +2,43 @@
 
 let
   cfg = config.services.ollama;
+  domain = config.networking.domain;
+  fqdn = if (cfg.subdomain != "") then "${cfg.subdomain}.${domain}" else domain;
 
-  inherit (lib) mkDefault mkIf;
+  inherit (lib)
+    mkDefault
+    mkIf
+    mkOption
+    types
+    ;
 in
 {
+  options.services.ollama = {
+    subdomain = mkOption {
+      type = types.str;
+      default = "ollama";
+      description = "Subdomain for Nginx virtual host. Leave empty for root domain.";
+    };
+    forceSSL = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Force SSL for Nginx virtual host.";
+    };
+  };
+
   config = mkIf cfg.enable {
     services.ollama = {
       user = mkDefault "ollama";
       group = mkDefault "ollama";
-      acceleration = mkDefault "cuda";
     };
+
+    services.nginx.virtualHosts."${fqdn}" = {
+      forceSSL = cfg.forceSSL;
+      enableACME = cfg.forceSSL;
+      locations."/".proxyPass = mkDefault "http://localhost:${toString cfg.port}";
+    };
+
+    security.acme.certs."${fqdn}".postRun = mkIf cfg.forceSSL "systemctl restart ollama.service";
 
     systemd.tmpfiles.rules = [
       "d ${cfg.models} 0755 ${cfg.user} ${cfg.group} -"
