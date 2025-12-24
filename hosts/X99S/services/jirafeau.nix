1a4368927c742fa1f083adde666dc09507b08405
stherm <steffen_hermann97@yahoo.de>
2025-12-24 09:20:25 +0100

    tested some changes in jirafeau

--- hosts/X99S/services/jirafeau.nix
+++ hosts/X99S/services/jirafeau.nix
@@ -1,25 +1,36 @@
 { inputs, config, ... }:
 
 let
-  cfg = config.services.jirafeau;
   tailnet = builtins.fromJSON (builtins.readFile ../../../tailnet.json);
+  cfg = config.services.jirafeau;
   fqdn = cfg.reverseProxy.subdomain + "." + tailnet.hosts.edge.publicDomain;
-
-  utils = import ../../../lib/utils.nix;
 in
 {
   imports = [ inputs.core.nixosModules.jirafeau ];
 
   services.jirafeau = {
     enable = true;
+
     reverseProxy = {
       enable = true;
       subdomain = "share";
       forceSSL = false;
     };
+    
+
+    nginxConfig = {
+      enableACME = false;
+      forceSSL = false;
+
+      listen = [
+        {
+          addr = tailnet.hosts."${config.networking.hostName}".address;
+          port = 80;
+        }
+      ];
+    };
+
   };
 
-  services.nginx.virtualHosts."${fqdn}" =
-    utils.mkInternalReverseProxy
-      tailnet.hosts."${config.networking.hostName}".address;
 }
+
