b7c10bccd8c75e3bfad8175513936f638875f0c0
stherm <steffen_hermann97@yahoo.de>
2026-01-10 18:11:33 +0100

    refactor(charbogen): remove manual nginx config as it is now handled by the module

--- hosts/X99S/services/charbogen.nix
+++ hosts/X99S/services/charbogen.nix
@@ -5,7 +5,7 @@
 
     enable = true;
     port = 6823;
-    domain = "www.role-dice.org";
+    domain = "www.roll-dice.org";
 
     secretKeyFile = config.sops.secrets."charbogen_flask_secret".path;
     dbUriFile = config.sops.secrets."charbogen_db_uri".path;
@@ -18,14 +18,5 @@
     "charbogen_db_creds" = { owner = "charbogen"; key = "charbogen/db_credentials"; };
     "charbogen_db_uri" = { owner = "charbogen"; key = "charbogen/db_uri"; };
     "charbogen_flask_secret" = { owner = "charbogen"; key = "charbogen/flask_secret"; };
-  };
-
-  services.nginx.virtualHosts."${config.services.charbogen.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.charbogen.port}";
-      proxyWebsockets = true;
-    };
   };
 }
