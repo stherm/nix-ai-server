27e392ed7009433c6d28a1efd7de6d75ec8d935c
stherm <steffen_hermann97@yahoo.de>
2026-01-10 18:14:31 +0100

    fix(charbogen): hotfix missing upstream nginx option and update port/config structure

--- hosts/X99S/services/charbogen.nix
+++ hosts/X99S/services/charbogen.nix
@@ -1,22 +1,32 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
-  services.charbogen = {
-
-    enable = true;
-    port = 6823;
-    domain = "www.roll-dice.org";
-
-    secretKeyFile = config.sops.secrets."charbogen_flask_secret".path;
-    dbUriFile = config.sops.secrets."charbogen_db_uri".path;
-    dbCredentialsFile = config.sops.secrets."charbogen_db_creds".path;
-    userCredentialsFile = config.sops.secrets."charbogen_user_creds".path;
+  options.services.charbogen.nginx = lib.mkOption {
+    type = lib.types.submodule {
+      options.enable = lib.mkEnableOption "nginx";
+    };
+    default = { enable = true; };
+    description = "HOTFIX: Define missing nginx option from upstream module";
   };
 
-  sops.secrets = {
-    "charbogen_user_creds" = { owner = "charbogen"; key = "charbogen/user_credentials"; };
-    "charbogen_db_creds" = { owner = "charbogen"; key = "charbogen/db_credentials"; };
-    "charbogen_db_uri" = { owner = "charbogen"; key = "charbogen/db_uri"; };
-    "charbogen_flask_secret" = { owner = "charbogen"; key = "charbogen/flask_secret"; };
+  config = {
+    services.charbogen = {
+      enable = true;
+      port = 5000;
+      domain = "www.roll-dice.org";
+      nginx.enable = true;
+
+      secretKeyFile = config.sops.secrets."charbogen_flask_secret".path;
+      dbUriFile = config.sops.secrets."charbogen_db_uri".path;
+      dbCredentialsFile = config.sops.secrets."charbogen_db_creds".path;
+      userCredentialsFile = config.sops.secrets."charbogen_user_creds".path;
+    };
+
+    sops.secrets = {
+      "charbogen_user_creds" = { owner = "charbogen"; key = "charbogen/user_credentials"; };
+      "charbogen_db_creds" = { owner = "charbogen"; key = "charbogen/db_credentials"; };
+      "charbogen_db_uri" = { owner = "charbogen"; key = "charbogen/db_uri"; };
+      "charbogen_flask_secret" = { owner = "charbogen"; key = "charbogen/flask_secret"; };
+    };
   };
 }
